1bb259ada5fb7888557b9a52514809a6c666e24b
Margie Nevarez <margie.ynm@gmail.com>
2017-08-01 19:37:00 -0700

    implement CRUD for movie model

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -3,4 +3,48 @@ class MoviesController < ApplicationController
     @movies = Movie.all
     render :index
   end
+
+  def show
+    @movie = Movie.find(params[:id])
+    render :show
+  end
+
+  def new
+    @movie = Movie.new
+    render :new
+  end
+
+  def create
+    @movie = Movie.new(movie_params)
+    if @movie.save
+      redirect_to movies_path
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @movie = Movie.find(params[:id])
+    render :edit
+  end
+
+  def update
+    @movie = Movie.find(params[:id])
+    if @movie.update(movie_params)
+      redirect_to movies_path
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @movie = Movie.find(params[:id])
+    @movie.destroy
+    redirect_to movies_path
+  end
+
+  private
+    def movie_params
+      params.require(:movie).permit(:name)
+    end
 end
